669968c240d6c3dd0f3e12c3cdb55d7918311c78
Nate Kearney <njk5023@gmail.com>
2018-08-29 18:26:00 -0400

    auth remove

--- src/main/java/com/techelevator/model/SmsSender.java
+++ src/main/java/com/techelevator/model/SmsSender.java
@@ -8,17 +8,12 @@ package com.techelevator.model;
 	public class SmsSender {
 		/*
 	    // Find your Account Sid and Auth Token at twilio.com/console
-	    public static final String ACCOUNT_SID =
-	            "ACc662cd062611460d4b264698fc7dec62";
-	    public static final String AUTH_TOKEN =
-	            "49dc4284ba5cfba87f9c1a2454d598d9";
-
 	    public static void main(String[] args) {
 	        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
 	        Message message = Message
-	                .creator(new PhoneNumber("+14126573458"), // to
-	                        new PhoneNumber("+18782187024"), // from
+	                .creator(new PhoneNumber("+1"), // to
+	                        new PhoneNumber("+1"), // from
 	                        "Where's Wallace?")
 	                .create();
 
